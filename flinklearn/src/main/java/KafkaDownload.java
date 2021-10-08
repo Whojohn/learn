9d452b2f567edaa4ddc4b191976647e62120ca28
Whojohn <18689235591@163.com>
2021-10-08 11:00:01 +0800

    Feat(flink):Add flink learn project.

---
+++ flinklearn/src/main/java/KafkaDownload.java
@@ -0,0 +1,47 @@
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class KafkaDownload {
+    public static void main(String[] args) throws Exception {
+        Properties properties = System.getProperties();
+        properties.setProperty("HADOOP_USER_NAME", "hadoop");
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment(2);
+        Properties kafkaProperties = new Properties();
+        Map<String, String> config = new HashMap<String, String>() {{
+            put("group.id", "test");
+            put("security.protocol", "SASL_PLAINTEXT");
+            put("sasl.mechanism", "PLAIN");
+            put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        }};
+        env.setParallelism(4);
+        env.enableCheckpointing(30000);
+
+
+        env.setStateBackend(new MemoryStateBackend(50 * 1024 * 1024));
+        config.keySet().forEach(e -> kafkaProperties.put(e, config.get(e)));
+        env.addSource(new FlinkKafkaConsumer<>("facebook", new SimpleStringSchema(), kafkaProperties).setStartFromEarliest())
+                .addSink(StreamingFileSink
+                        .forRowFormat(new Path("hdfs://test:9000/test/facebook"), new SimpleStringEncoder<String>("UTF-8"))
+                        .withRollingPolicy(
+                                DefaultRollingPolicy.builder()
+                                        .withRolloverInterval(TimeUnit.MINUTES.toMillis(2))
+                                        .withInactivityInterval(TimeUnit.MINUTES.toMillis(2))
+                                        .withMaxPartSize(1024 * 1024 * 1024)
+                                        .build())
+                        .build());
+        env.execute();
+
+    }
+}
---
+++ flinklearn/src/main/java/WordCount.java
@@ -0,0 +1,39 @@
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.RestOptions;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.types.StringValue;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        // 访问http://localhost:8082 可以看到Flink Web UI
+        conf.setInteger(RestOptions.PORT, 8082);
+        // 创建本地执行环境，并行度为2
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment(2, conf);
+
+
+        env.setRuntimeMode(RuntimeExecutionMode.BATCH);
+        env.setParallelism(1);
+        env.readTextFile("hdfs://test:9000/paralli/news2016zh_train.json").map(new MapFunction<String, Tuple2<StringValue, Integer>>() {
+            @Override
+            public Tuple2<StringValue, Integer> map(String value) throws Exception {
+                new ObjectMapper().readTree(value).get("");
+
+                return new Tuple2<StringValue, Integer>(new StringValue("a"),
+                        new Integer(1));
+            }
+        }).keyBy(0).reduce(new ReduceFunction<Tuple2<StringValue, Integer>>() {
+            @Override
+            public Tuple2<StringValue, Integer> reduce(Tuple2<StringValue, Integer> value1, Tuple2<StringValue, Integer> value2) throws Exception {
+                return new Tuple2(value1.f0, value1.f1 + value2.f1);
+            }
+        }).print();
+        env.execute();
+    }
+}
